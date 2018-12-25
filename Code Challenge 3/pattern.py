4168af603c8815c7a041d50092a1ab852b4d34e3
Piyush Kumar <36813120+piyush546@users.noreply.github.com>
2018-12-25 13:08:58 +0530

    Update and rename pattern.py to Code Challenge 3/pattern.py

--- Code Challenge 3/pattern.py
+++ Code Challenge 3/pattern.py
@@ -5,5 +5,18 @@ columns = int(input())
 # Printing the pattern
 for i in range(1, columns+1):
     print("*"*i)
-for j in range(columns-1, 0,-1):
+for j in range(columns-1, 0, -1):
     print("*"*j)
+
+# Input = 5
+""" Output =
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+"""
