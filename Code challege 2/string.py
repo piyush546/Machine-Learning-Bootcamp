28f7e2295a8369c2ca7a19cc0caff0f7d961cbf1
Piyush Kumar <36813120+piyush546@users.noreply.github.com>
2018-12-25 11:38:15 +0530

    Update string.py

--- Code challege 2/string.py
+++ Code challege 2/string.py
@@ -3,7 +3,10 @@
 # To take user name input in single command using split
 first_name, last_name = input().split(" ")
 
-# to print reverse the string
+# to print the name string after reversing
 full_name = first_name+" "+last_name
 j = full_name.index(" ")
-print(full_name[j+1:len(full_name)+1]+" "+full_name[0:j])
+print(full_name[j+1:len(full_name)+1]+full_name[j]+full_name[0:j])
+
+# Input = piyush kumar
+# Output = kumar piyush
