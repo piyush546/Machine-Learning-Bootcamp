b12e7435b5f4eb91395609523b92e04fcac0092c
Piyush Kumar <36813120+piyush546@users.noreply.github.com>
2018-12-25 09:53:49 +0530

    Update and rename format3.py to Code challenge 2/format3.py

--- Code challenge 2/format3.py
+++ Code challenge 2/format3.py
@@ -7,3 +7,6 @@ String1  =  String1.replace('', '*')
 
 # printing the formatted string
 print(String1)
+
+# Input = Welcome to Pink City Jaipur!
+# Output = *W*e*l*c*o*m*e* *t*o* *P*i*n*k* *C*i*t*y* *J*a*i*p*u*r*!*
