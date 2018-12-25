f8c112d76e460a06edec65d1bfddf9582d0d3981
Piyush Kumar <36813120+piyush546@users.noreply.github.com>
2018-12-25 09:51:15 +0530

    Add files via upload
    code challenge 2

---
+++ format3.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Variable to store the inputed string
+String1 = input()
+
+# formatting the string
+String1  =  String1.replace('', '*')
+
+# printing the formatted string
+print(String1)
