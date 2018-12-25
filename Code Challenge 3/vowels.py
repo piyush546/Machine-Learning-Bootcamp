cf845a93772f5359ff22fe023356a8f1798aa154
Piyush Kumar <36813120+piyush546@users.noreply.github.com>
2018-12-25 12:54:31 +0530

    Add files via upload

---
+++ Code Challenge 3/vowels.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# List containing the vowels and the states
+list1 = ['a', 'e', 'i', 'o', 'u']
+state_name = ['Alabama', 'California', 'Oklahoma', 'Florida']
+
+# Process to remove the vowels
+for i in range(0, len(state_name)):
+    replica_state = str.lower(state_name[i])
+    replica_state = list(replica_state)
+    d = len(state_name[i])
+    for j in range(0, d):
+        if (replica_state[j] in list1):
+            replica_state[j] = ''
+        else:
+            continue
+    replica_state = ''.join(replica_state)
+    state_name[i] = str.capitalize(replica_state)
+
+# printing the modified list
+print("Modified list:", state_name)
