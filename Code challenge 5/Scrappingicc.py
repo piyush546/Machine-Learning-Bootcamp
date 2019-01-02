d183bf1bea0658c169a42f6f9541035848a2ed10
Piyush Kumar <36813120+piyush546@users.noreply.github.com>
2019-01-02 13:19:33 +0530

    Update Scrappingicc.py

--- Code challenge 5/Scrappingicc.py
+++ Code challenge 5/Scrappingicc.py
@@ -39,18 +39,19 @@ E = []
 for rows in required_table.findAll("tr"):
     data = rows.findAll("td")
     if(len(data) == 5):
-        print(A.append(data[0].find(text=True)))
-        print(B.append(data[1].find(text=True)))
-        print(C.append(data[2].find(text=True)))
-        print(D.append(data[3].find(text=True)))
-        print(E.append(data[4].find(text=True)))
+        print(A.append(data[0].text.strip()))
+        print(B.append(data[1].text.strip()))
+        print(C.append(data[2].text.strip()))
+        print(D.append(data[3].text.strip()))
+        print(E.append(data[4].text.strip()))
 
 # froming dataframe
+head = [i.text.strip() for i in required_table.find_all("th")]
 df = pd.DataFrame()
-df['Pos'] = A
-df['Team'] = B
-df['Weighted Matches'] = C
-df['Points'] = D
-df['Rating'] = E
-df.to_csv("former3.csv")
+df[head[0]] = A
+df[head[1]] = B
+df[head[2]] = C
+df[head[3]] = D
+df[head[4]] = E
+df.to_csv("former3.csv", index=False)
 print(df)
