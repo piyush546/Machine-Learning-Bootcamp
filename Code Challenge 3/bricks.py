444ac9a50565bb5b41590f430a6e32aae1c0150b
Piyush Kumar <36813120+piyush546@users.noreply.github.com>
2019-01-11 13:23:16 +0530

    Update bricks.py

--- Code Challenge 3/bricks.py
+++ Code Challenge 3/bricks.py
@@ -1,36 +1,32 @@
 # -*- coding: utf-8 -*-
 # To take input for number of bricks and target sixe[1-inch,5-inch,target]
-list2 = list(map(int, input().split(' ')))
+list2 = input().split(",")
 
 
 # A function to check whether target size can be achieved or not
 def check_func(list_repl):
-    target = list_repl[2]
-    while ((list_repl[1] != 0 or list_repl[0] != 0) and target > 0):
+    target = int(list_repl[2])
+    while ((int(list_repl[1]) != 0 or int(list_repl[0]) != 0) and target > 0):
         if (target >= 5):
-            if (list_repl[1] != 0):
+            if (int(list_repl[1]) != 0):
                 target = target-5
-                list_repl[1] = list_repl[1]-1
-                print(target)
+                list_repl[1] = int(list_repl[1])-1
         else:
-            if(list_repl[0] != 0):
+            if(int(list_repl[0]) != 0):
                 target = target-1
-                list_repl[0] = list_repl[0]-1
-                print(target)
+                list_repl[0] = int(list_repl[0])-1
 
     if (target == 0):
-        print("True")
+        return "True"
     else:
-        print("False")
+        return "False"
 
 
 # function call
-check_func(list2)
+print(check_func(list2))
 
 
 # input = 2 2 11
 """output:
-6
-1
-0
 True"""
+
