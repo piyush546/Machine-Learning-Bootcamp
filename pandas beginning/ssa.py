a8ff6cafa4d37c75adad4ca07afc0ca9752f0a1a
piyush546 <pk62893@gmail.com>
2019-02-19 17:46:24 +0530

    updated

--- pandas beginning/ssa.py
+++ pandas beginning/ssa.py
@@ -21,16 +21,46 @@ import matplotlib.pyplot as plt
 
 
 # Defining a function for performing various calculations on the dataframes column
-"""def frames_filter(dataframe_col[year]):
-    dataframe_col[year] = dataframe_col[year].str.split(",", expand=True).add_prefix("col_")
-    dataframe_col['col_2'] = datframe_col['col_2'].apply(int)
-    female_data = dataframe_col[dataframe_col['col_1']=="F"].sort_values('col_2', ascending=True).head()
-    male_data = dataframe_col[dataframe_col['col_1']=="M"].sort_values('col_2', ascending=True).head()
-    sum_year = dataframe_col['col_2'].sum()
-    gender_sum = pd.pivot_table(dataframe_col,values=['col_2'], columns=['col_1'], aggfunc=np.sum)
-    plt.pie([sum_year, gender_sum['F'], gender_sum['M']], explode=[0, 0, 0.1], labels=['total','female','male'], autopct="%1.1f%%")
-    plt.axis("equals")
-    return plt.show()"""
+def frames_filter(dataframe_col,year_repl):
+
+    # Removing the null values from the DataFrames as null values will generate error while we use .apply(int) method
+    dataframe_col = ssa_dataframe[2010].dropna()
+
+    # Fetching top 5 males and Females baby names
+
+    # and droping the null value containing rows
+    # Series.str.split(delimeter,n,expand) - Split strings around given separator/delimiter.
+    # .add_prefix(str) add labels to the dataframe. I used it to name the column by lable col_
+    dataframe_col_re= dataframe_col[year_repl].str.split(",", expand=True).add_prefix("col_")
+
+    # pandas.DataFrame.apply(funcn) - to apply function on dataframe
+    # I used it here to apply int function on a column containing numeral values
+    # but in str format. Using this I converted the type from str to int
+    dataframe_col_re.iloc[:, -1] = dataframe_col_re.iloc[:, -1].apply(int)
+
+    # Sorting the female data accprding to counts using sort_values() method
+    # giving false to ascending parameter in sort_values sorts the data in descending order
+    # .head() fetch 5 values from the start of the dataframe
+    # Female_data the top 5 female baby names
+    female_data = dataframe_col_re[dataframe_col_re['col_1']=="F"].sort_values('col_2', ascending=True).head()
+
+    # Top 5 male baby names
+    male_data = dataframe_col_re[dataframe_col_re['col_1']=="M"].sort_values('col_2', ascending=True).head()
+
+    # Sum accounting the total_numbers of babies
+    sum_year = dataframe_col_re['col_2'].sum()
+
+    # Sum accounting to male and female babies counts
+    gender_sum = pd.pivot_table(dataframe_col_re,values=['col_2'], columns=['col_1'], aggfunc=np.sum)
+
+    # Plotting the pictorial pie chart for the sum of counts for whole and accoridng to gender
+    plt.pie([gender_sum['F'], gender_sum['M']], explode=[0, 0], labels=['female','male'], autopct="%1.1f%%")
+
+    # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.axis("equal")
+
+    # For displaying the plotted pie chart
+    return plt.show()
 # Exception handling for the Dataframe exceptions
 try:
     # Initializing the parent dataframe
@@ -68,46 +98,15 @@ try:
         count = count+1
     ssa_dataframe = pd.concat(dataframe_list, axis=1)
 
+    # Function call to analyze the fetched data
+    frames_filter(ssa_dataframe, 2010)
+
 except TypeError as e:
     print(e)
 
 except ValueError as e:
     print(e)
 
-# ************************************************* #
-# Fetching top 5 males and Females baby names
+except AttributeError as e:
+    print(e)
 
-# Fetching 2010 records in a variable data_2010 with filtering it
-# and droping the null value containing rows
-# Series.str.split(delimeter,n,expand) - Split strings around given separator/delimiter.
-# .add_prefix(str) add labels to the dataframe. I used it to name the column by lable col_
-data_set = data_set[2010].str.split(",", expand=True).add_prefix('col_')
-
-# pandas.DataFrame.apply(funcn) - to apply function on dataframe
-# I used it here to apply int function on a column containing numeral values
-# but in str format. Using this I converted the type from str to int
-data_set.iloc[:, -1] = data_set.iloc[:, -1].apply(int)
-
-# Sorting the female data accprding to counts using sort_values() method
-# giving false to ascending parameter in sort_values sorts the data in descending order
-# .head() fetch 5 values from the start of the dataframe
-# Female_data the top 5 female baby names
-Female_data = data_set[data_set['col_1'] == 'F'].sort_values('col_2', ascending=False).head()
-
-# Top % male baby names
-Male_data = data_set[data_set['col_1'] == 'M'].sort_values('col_2', ascending=False).head()
-
-# Sum accounting the total_numbers of babies
-sum_2010 = data_set['col_2'].sum()
-
-# Sum accounting to male and female babies counts
-gender_sum_2010 = pd.pivot_table(data_set, values='col_2', columns=['col_1'], aggfunc=np.sum)
-
-# Plotting the pictorial pie chart for the sum of counts for whole and accoridng to gender
-plt.pie([sum_2010, gender_sum_2010['F'], gender_sum_2010['M']], explode=[0, 0, 0.1], labels=['total', 'female', 'male'], autopct='%1.1f%%', shadow=True)
-
-# Equal aspect ratio ensures that pie is drawn as a circle.
-plt.axis('equal')
-
-# For displaying the plotted pie chart
-plt.show()
