d55755a0e9b3830e95cfb75d32dc8b4727f115c1
piyush546 <pk62893@gmail.com>
2019-02-21 18:37:48 +0530

    updated

--- pandas beginning/bitly.py
+++ pandas beginning/bitly.py
@@ -12,56 +12,61 @@ import numpy as np
 # I imported collections.Counter to check the timezones frequency
 from collections import Counter
 
-# Importing regex module
-import re
-# Importing matplotlib.pyplot for pictorial representation of the stats
-# import matplotlib.pyplot as plt
+try:
+    # Creating the dataframe of the json file containing all the url shortener hits
+    # pandas.read_json is convert json string to pandas object
+    # lines parameter reads the file as json object per line
+    json_df = pd.read_json("usagov_bitly_data.json", lines=True)
 
-# Creating the dataframe of the json file containing all the url shortener hits
-# pandas.read_json is convert json string to pandas object
-# lines parameter reads the file as json object per line
-json_df = pd.read_json("usagov_bitly_data.json", lines=True)
+    # Repalcing some data with some other data
+    # pandas.DataFrame.replace(to_replace=None, value=None,) - Replace values given in to_replace with 'value'.
+    json_df = json_df.replace([np.nan, ""], ["Mising", "Unknown"])
 
-# Repalcing some data with some other data
-# pandas.DataFrame.replace(to_replace=None, value=None,) - Replace values given in to_replace with 'value'.
-json_df = json_df.replace([np.nan, ""], ["Mising", "Unknown"])
+    # Getting the top 10 timezones frequency using pandas method
+    json_df_tz = json_df['tz'].value_counts().head(10)
 
-# Getting the top 10 timezones frequency using pandas method
-json_df_tz = json_df['tz'].value_counts().head(10)
+    # Getting the top 10 timezones frequency not using pandas method
+    json_tz = Counter(json_df['tz'])
 
-# Getting the top 10 timezones frequency not using pandas method
-json_tz = Counter(json_df['tz'])
+    json_tz = sorted(json_tz.items(), key=lambda x: x[1], reverse=True)
 
-json_tz = sorted(json_tz.items(), key=lambda x: x[1], reverse=True)
+    json_tz = json_tz[:10]
 
-json_tz = json_tz[:10]
+    # Getting frequency of each timezones
+    tz_count = json_df['tz'].value_counts()
 
-# Getting frequency of each timezones
-tz_count = json_df['tz'].value_counts()
+    # To draw the top 10 timezones frequnecy bar graph for a series we can use Series.plot.type()
+    json_df_tz.plot.bar()
 
-# To draw the top 10 timezones frequnecy bar graph for a series we can use Series.plot.type()
-json_df_tz.plot.bar()
+    # ************************************ #
+    # Fetching the browser capability i.e the first token (^\w+\/[\d\.\d]*)
 
-# ************************************ #
-# Fetching the browser capability i.e the first token (^\w+\/[\d\.\d]*)
+    # Spiliting the Fetched series of browser info. according to tokens
+    tokens_df = json_df['a'].str.split(n=1, expand=True).add_prefix("Token_")
 
-# Spiliting the Fetched series of browser info. according to tokens
-tokens_df = json_df['a'].str.split(n=1, expand=True).add_prefix("Token_")
+    # Fetching the frequency of the browser capability
+    tokens_frequency = tokens_df['Token_0'].value_counts()
 
-# Fetching the frequency of the browser capability
-tokens_frequency = tokens_df['Token_0'].value_counts()
+    # Plotting bar graph for top 5 browser capability
+    tokens_frequency.head().plot.bar()
 
-# Plotting bar graph for top 5 browser capability
-tokens_frequency.head().plot.bar()
+    # Filling the missing values in the token parts with mising string
+    tokens_df = tokens_df.replace(np.nan, 'Mising')
 
-# Filling the missing values in the token parts with mising string
-tokens_df = tokens_df.replace(np.nan, 'Mising')
+    # ******************************************** #
+    # Classifying as windows os and non-windows os
 
-# ******************************************** #
-# Classifying as windows os and non-windows os
+    # Initializing the os column as Not windows
+    tokens_df["os"] = 'Not Windows'
 
-# Initializing the os column as Not windows
-tokens_df["os"] = 'Not Windows'
+    # Applying the conditions to find the windows os user and initializing their os column as Windows
+    tokens_df["os"][tokens_df["Token_1"].str.find("Windows") != -1] = "Windows"
 
-# Applying the conditions to find the windows os user and initializing their os column as Windows
-tokens_df["os"][tokens_df["Token_1"].str.find("Windows") != -1] = "Windows"
+except ValueError as e:
+    print(e)
+
+except AttributeError as e:
+    print(e)
+
+except TypeError as e:
+    print(e)
