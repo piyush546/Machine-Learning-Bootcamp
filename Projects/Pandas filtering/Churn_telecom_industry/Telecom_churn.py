3565b13a20e2293861282b6b4827d4e5e70fb310
@piyush546 <pk62893@gmail.com>
2019-02-27 11:02:52 +0530

    updated

---
+++ Projects/Pandas filtering/Churn_telecom_industry/Telecom_churn.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+""" """
+# Import pandas, numpy, matplotlib
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Loading the datasets containing the Telecom churn data
+churn_df = pd.read_csv("Telecom_churn.csv")
