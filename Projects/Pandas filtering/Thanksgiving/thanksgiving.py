d592ca06d30b86523624da1a039f4bbb933f9904
@piyush546 <pk62893@gmail.com>
2019-03-04 08:52:25 +0530

    updated

--- Projects/Pandas filtering/Thanksgiving/thanksgiving.py
+++ Projects/Pandas filtering/Thanksgiving/thanksgiving.py
@@ -9,6 +9,26 @@ import matplotlib.pyplot as plt
 # Importing contextlib module to handle exception silently
 import contextlib
 
-with contextlib.suppress((FileNotFoundError,UnicodeDecodeError)):
+with contextlib.suppress((FileNotFoundError, UnicodeDecodeError, NameError)):
     # Loading the thanksgiving dataset
-    thanks_df = pd.read_csv("thanksgiving.csv", encoding="")
+    thanks_df = pd.read_csv("thanksgiving.csv", encoding="Windows 1252")
+
+    # Fetching the column names in a list
+    columns_list = list(thanks_df.columns)
+
+    # Analysing how much celebrate thanksgiving
+    thanksgiving_anly = thanks_df[columns_list[1]].value_counts()
+
+    # Analysing the main dish for thanksgiving
+    dish_anly = thanks_df[columns_list[2]].value_counts()
+
+    # Analysing the salary range
+    salary_anly = thanks_df[columns_list[63]].value_counts()
+
+    # Analysing the regions
+    states_anly = thanks_df.iloc[:,-1].value_counts()
+
+    # getting states analysis for thanksgiving celebration
+    states_thanks_anly = thanks_df.groupby([columns_list[64], columns_list[63], columns_list[1]])
+
+
