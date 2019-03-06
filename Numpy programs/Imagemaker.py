156c88c71787d3256b748a909a202fabd9836dd3
@piyush546 <pk62893@gmail.com>
2019-03-06 18:19:17 +0530

    updated

--- Numpy programs/Imagemaker.py
+++ Numpy programs/Imagemaker.py
@@ -7,42 +7,20 @@ import matplotlib.pyplot as plt
 
 from PIL import Image
 
-img = np.full((8,8), 5.43355)
-img[0][2] = 1
-img[0][3] = 1
-img[0][4] = 1
-img[0][5] = 1
-img[7][2] = 1
-img[7][3] = 1
-img[7][4] = 1
-img[7][5] = 1
+img = np.ones((8, 8))
 
-img[1][1] = 1
-img[6][1] = 1
-
-img[2][0] = 1
-img[3][0] = 1
-img[4][0] = 1
-img[5][0] = 1
-img[2][7] = 1
-img[3][7] = 1
-img[4][7] = 1
-img[5][7] = 1
-
-img[1][6] = 1
-img[6][6] = 1
-
-img[2][2] = 1
-img[2][5] = 1
-
-img[4][2] = 1
-img[4][5] = 1
-
-img[5][3] = 1
-img[5][4] = 1
+img = [[1, 1, 0, 0, 0, 0, 1, 1],
+       [1, 0, 1, 1, 1, 1, 0, 1],
+       [0, 1, 0, 1, 1, 0, 1, 0],
+       [0, 1, 1, 1, 1, 1, 1, 0],
+       [0, 1, 0, 1, 1, 0, 1, 0],
+       [0, 1, 1, 0, 0, 1, 1, 0],
+       [1, 0, 1, 1, 1, 1, 0, 1],
+       [1, 1, 0, 0, 0, 0, 1, 1]]
 
 plt.imshow(img, "gray")
 
-img_file = Image.fromarray(img, "RGB")
+
+img_file = Image.fromarray(img, "I")
 img_file.save("Smile.png")
 img_file.show()
