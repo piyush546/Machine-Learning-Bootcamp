351b5c67259b994791369cde225399ff3984687c
@piyush546 <pk62893@gmail.com>
2019-03-13 07:59:33 +0530

    updated

--- Machine Learning programs/Supervised ML programs/Linear Regression Programs/MultiVariant Linear Regression/Atlantapumpkin_Analysis.py
+++ Machine Learning programs/Supervised ML programs/Linear Regression Programs/MultiVariant Linear Regression/Atlantapumpkin_Analysis.py
@@ -1,2 +1,17 @@
 # -*- coding: utf-8 -*-
 
+import pandas as pd
+pumpkin_df = pd.read_csv("atlanta_pumpkin.csv")
+col = list(pumpkin_df.columns)
+dataset = pumpkin_df.loc[:,[col[10],col[11],col[14]]]
+
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+features = dataset.iloc[:,:-1].values
+labels = dataset.iloc[:,-1].values.reshape(-1,1)
+labels = LabelEncoder().fit_transform(labels)
+regressor = LinearRegression()
+regressor.fit(features, labels)
+dataset.loc[:,['col']] = 'missing'
