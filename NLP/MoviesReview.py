0060f60cd52bfec6e671e5217f540d9839ad4157
@piyush546 <pk62893@gmail.com>
2019-04-19 11:22:30 +0530

    updated

--- NLP/MoviesReview.py
+++ NLP/MoviesReview.py
@@ -18,6 +18,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from itertools import count
+
 # Loading dataset
 dataset = pd.read_csv("movie.csv")
 
@@ -34,7 +36,7 @@ dataset['class'] = np.where(dataset['class'] == 'Pos', 1, 0)
 corpus = []
 from nltk.stem.porter import PorterStemmer
 ps = PorterStemmer() 
-for var in range(0,2000):
+for var in count(0):
     review = re.sub('[^a-zA-Z]',' ', dataset['text'][var])
     review = review.lower()
     review = review.split()
@@ -43,6 +45,57 @@ for var in range(0,2000):
     review = [ps.stem(words) for words in review]    
     review = " ".join(review)
     corpus.append(review)
-
+    if var == 2000:
+        break
 # Formimg bag of words model
 from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+features = cv.fit_transform(corpus).toarray()
+labels = dataset.iloc[:,0].values
+
+# Seperating training and testing data
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.2, random_state=0)
+
+# Applying the classification algorithm
+from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB
+
+
+# Applying Different Naive Bayes
+
+gnb =  GaussianNB()
+gnb.fit(features_train, labels_train)
+labels_pred = gnb.predict(features_test)
+print("GaussianNB score:",gnb.score(features_test, labels_test))
+
+############################################
+
+mnb =  MultinomialNB()
+mnb.fit(features_train, labels_train)
+labels_pred_1 = mnb.predict(features_test)
+print("MultinomialNB:", mnb.score(features_test, labels_test))
+
+###################################################
+
+bnb =  BernoulliNB()
+bnb.fit(features_train, labels_train)
+labels_pred_2 = bnb.predict(features_test)
+print("BernoulliNB:", bnb.score(features_test, labels_test))
+
+##############################################
+
+# Applying the KNN classifiaction algorithm
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier()
+knn.fit(features_train, labels_train)
+labels_pred_3 = knn.predict(features_test)
+print("Knn score:", knn.score(features_test, labels_test))
+
+##################################################
+
+# Framing confusion matrix
+from sklearn.metrics import confusion_matrix 
+cm_gnb = confusion_matrix(labels_test, labels_pred)
+cm_mnb = confusion_matrix(labels_test, labels_pred_1)
+cm_bnb = confusion_matrix(labels_test, labels_pred_2)
+cm_knn = confusion_matrix(labels_test, labels_pred_3)
