ed13e257502f3fbc343ffb8adaefaba4fabc5476
@piyush546 <pk62893@gmail.com>
2019-05-21 15:23:04 +0530

    updated

--- Numpy programs/Automobile.py
+++ Numpy programs/Automobile.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
-""" A program to performm numpy operations on a csv file like array generation,
-std.deviation etc """
+"""
+Code Challenge
+  Name: 
+      Exploratory Data Analysis - Automobile
+  Filename: 
+      automobile.py
+  Dataset:
+      Automobile.csv
+  Problem Statement:
+      Perform the following task :
+      1. Handle the missing values for Price column
+      2. Get the values from Price column into a numpy.ndarray
+      3. Calculate the Minimum Price, Maximum Price, Average Price and Standard Deviation of Price
+"""
 
 # Importing pandas and numpy module
 import pandas as pd
