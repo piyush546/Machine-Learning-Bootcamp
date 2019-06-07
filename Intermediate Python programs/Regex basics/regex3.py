bcefdbed47526e5963885bb84835151a2fc44844
@piyush546 <pk62893@gmail.com>
2019-06-07 09:59:59 +0530

    updated

--- Intermediate Python programs/Regex basics/regex3.py
+++ Intermediate Python programs/Regex basics/regex3.py
@@ -10,12 +10,31 @@ for i in range(0, n):
     List.append(var1)
 
 # Required pattern
-regex = re.compile("^[456][0-9]{15}|^[456][0-9]{3}-[0-9]{4}-[0-9]{4}-[0-9]{4}")
+regex = re.compile("^[456]([0-9]{15}|[0-9]{3}-([0-9]{4}-?){3})")
 
 # processing the result
 for var in range(0, len(List)):
     result = regex.match(List[var])
-    if result is None:
-        print("Invalid")
+    if result:
+        if not(re.search(r"(\d)\1{3,}", List[var])):
+            print("valid")
+        else:
+            print("Invalid")
     else:
-        print("Valid")
+        print("Invalid")
+
+"""
+4123456789123456
+5123-4567-8912-3456
+61234-567-8912-3456
+4123356789123456
+5133-3367 -8912-3456
+5123 - 3567 - 8912 - 3456
+    
+Valid
+Valid
+Invalid
+Valid
+Invalid
+Invalid
+"""
