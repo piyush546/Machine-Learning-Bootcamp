086c9c04fac7d28d404aac54c4aff36b05a96ef7
@piyush546 <pk62893@gmail.com>
2019-06-12 03:18:28 +0530

    updated

--- Data preprocessing and analytics/pandas and matplotlib/election_analysis/election_analysis.py
+++ Data preprocessing and analytics/pandas and matplotlib/election_analysis/election_analysis.py
@@ -4,60 +4,79 @@ code challenge - election data
 
 1. Fetch the top parties of each state within each constituency with their vote %.
 
-2. Visualize the above result for the biggest state UP.
+2. Visualize the top parties vote % in each constituency for Rajasthan.
 
 3. Visualize the total seats gained by each party in each states.
 
-4. Calculate the total votes casted in each state.
+4. Visualize the total seats won by the parties in the whole country
 
 """
 
-# Data preprocessing modules
+# Data preprocessing and visualization modules
 import pandas as pd
-import numpy as np
-import seaborn
+# import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 
-
-dataset = pd.read_csv("election.csv")
-dataset = dataset.loc[:,['Candidate', 'Party','State', 'Constituency', 'Total Votes', '%']]
-
-
-mod_data = dataset.sort_values("%").drop_duplicates(["State","Constituency"], keep="last")
-mod_data = mod_data.sort_values("State")
-mod_data = mod_data.iloc[:,[2,3,1,5]]
-mod_data["State"] = (mod_data["State"]+","+mod_data["Constituency"]+","+mod_data["Party"])
-mod_data.index = [x for x in range(0,542)]
-mod_data = mod_data.iloc[:,[0,-1]]
-
-seaborn.barplot(mod_data.iloc[:,1],mod_data.iloc[:,0])
-
-new_mod_data = dataset.sort_values("%").drop_duplicates(["State","Constituency"], keep="last")
-new_mod_data.index = [x for x in range(0,542)]
-new_mod_data["State"] = new_mod_data["State"]+","+new_mod_data["Party"]
-new_mod_data = new_mod_data["State"]
-new_mod_data = new_mod_data.value_counts()
-
-plt.figure(figsize=(4,30))
-seaborn.barplot(new_mod_data, new_mod_data.index)
-plt.xlabel("No.of seats")
-plt.ylabel("state and party")
-plt.show()
-data = mod_data['State'].str.split(",", expand=True).add_prefix("col_")
-
-
-#% matplotlib.inline
+# Plotly module for better visualization
 from plotly.offline import init_notebook_mode, plot #, iplot, download_plotlyjs
 import plotly.graph_objs as go
 init_notebook_mode(connected=True)
-trace=go.Pie(labels=list(data["col_2"].value_counts().index),values=list(data["col_2"].value_counts()))
-#bar = go.Bar(y=mod_data.iloc[:,1], x=mod_data.iloc[:,0])
-#layout = go.Layout(title="parties distribution in each constituency",autosize=False, width=1000, height=600)
-#fig = go.Figure(data=[trace], layout=layout)
-#plot(fig)
+
+# Loading the data
+dataset = pd.read_csv("election.csv")
+
+# Selecting the important features for further processing
+dataset = dataset.loc[:,['Candidate', 'Party','State', 'Constituency', 'Total Votes', '%']]
+
+# State -> Constituency -> Top parties with their vote percentage
+state_con_data = dataset.sort_values("%").drop_duplicates(["State","Constituency"], keep="last")
+state_con_data = state_con_data.sort_values("State")
+state_con_data = state_con_data.iloc[:,[2,3,1,5]]
+state_con_data["State"] = (state_con_data["State"]+","+state_con_data["Constituency"]+","+state_con_data["Party"])
+state_con_data.index = [x for x in range(0,542)]
+state_con_data = state_con_data.iloc[:,[0,-1]]
+
+# Visualization of Rajasthan state constituency
+sns.barplot(state_con_data.iloc[332:356,0],state_con_data.iloc[332:356,1])
+plt.xticks(rotation=90)
+plt.title("Rajasthan consitutency wise party win %")
+plt.xlabel("State,Constituency,party")
+plt.ylabel("vote %")
+plt.grid(True)
+plt.show()
+
+"""
+bar = go.Bar(y=state_con_data.iloc[:,1], x=state_con_data.iloc[:,0])
+layout = go.Layout(title="parties distribution in each constituency",autosize=False, width=1000, height=600)
+fig = go.Figure(data=[bar], layout=layout)
+plot(fig)
+"""
+
+# State -> party seat count
+state_party_data = dataset.sort_values("%").drop_duplicates(["State","Constituency"], keep="last")
+state_party_data.index = [x for x in range(0,542)]
+state_party_data["State"] = state_party_data["State"]+","+state_party_data["Party"]
+state_party_data = state_party_data["State"]
+state_party_data = state_party_data.value_counts()
+
+# Visualizing the parties in each state with their seats won
+plt.figure(figsize=(4,30))
+sns.barplot(state_party_data, state_party_data.index)
+plt.title("Top Parties in Each state")
+plt.xlabel("No.of seats won")
+plt.ylabel("state and party")
+plt.grid(True)
+plt.show()
+
+
+# Visualizations of total seats won by each party in whole country
+total_seats = state_con_data['State'].str.split(",", expand=True).add_prefix("col_")
+trace=go.Pie(labels=list(total_seats["col_2"].value_counts().index),values=list(total_seats["col_2"].value_counts()))
 plot([trace])
 #iplot([trace])
 
+# Fetching the States on basis of directions to show region wise trend of parties win
 north = ['Jammu & Kashmir', 'Himachal Pradesh', 'Punjab', 'Uttarakhand' , 'Uttar Pradesh',
          'Haryana', 'Chandigarh','NCT OF Delhi']
 east  = [ 'Bihar', 'Odisha', 'Jharkhand', 'West Bengal']
@@ -71,14 +90,14 @@ north_east = ['Assam', 'Sikkim', 'Nagaland', 'Meghalaya', 'Manipur', 'Mizoram',
 
 
 
-data = new_mod_data.reset_index()
-seats = data["State"]
-data[["State","Party"]] = data["index"].str.split(",", expand=True)
-data["index"] = seats
-data.columns = ["seats", "state","party"]
-data = data.sort_values("seats").drop_duplicates(["state"], keep="last")
-data.index =  [x for x in range(0, 36)]
-states = data["state"].tolist()
+region_wise_trend = state_party_data.reset_index()
+seats = region_wise_trend["State"]
+region_wise_trend[["State","Party"]] = region_wise_trend["index"].str.split(",", expand=True)
+region_wise_trend["index"] = seats
+region_wise_trend.columns = ["seats", "state","party"]
+region_wise_trend = region_wise_trend.sort_values("seats").drop_duplicates(["state"], keep="last")
+region_wise_trend.index =  [x for x in range(0, 36)]
+states = region_wise_trend["state"].tolist()
 
 def mod(var):
     if var in north:
@@ -95,92 +114,9 @@ def mod(var):
         return "north_east"
     else:
         return "missing"
-data['region'] = data['state'].map(mod)
+region_wise_trend['region'] = region_wise_trend['state'].map(mod)
 
-data = data.sort_values("region")
-
-pie = {'data':[{
-        'labels':data["party"][data["region"]=="North"],
-        'values':data["seats"][data["region"]=="North"],
-        "text":["Election"],
-      "textposition":"inside",
-      "domain": {"column": 1},
-      "name": "Party Trend",
-      "hoverinfo":"label+percent+name",
-      "hole": .4,
-      "type": "pie"},
-    {
-        'labels':data["party"][data["region"]=="East"],
-        'values':data["seats"][data["region"]=="East"],
-        "text":["Election"],
-      "textposition":"inside",
-      "domain": {"column": 1},
-      "name": "Party Trend",
-      "hoverinfo":"label+percent+name",
-      "hole": .4,
-      "type": "pie"},
-     {
-        'labels':data["party"][data["region"]=="west"],
-        'values':data["seats"][data["region"]=="west"],
-        "text":["Election"],
-      "textposition":"inside",
-      "domain": {"column": 1},
-      "name": "Party Trend",
-      "hoverinfo":"label+percent+name",
-      "hole": .4,
-      "type": "pie"},
-      {
-        'labels':data["party"][data["region"]=="South"],
-        'values':data["seats"][data["region"]=="South"],
-        "text":["Election"],
-      "textposition":"inside",
-      "domain": {"column": 1},
-      "name": "Party Trend",
-      "hoverinfo":"label+percent+name",
-      "hole": .4,
-      "type": "pie"},
-       {
-        'labels':data["party"][data["region"]=="Central"],
-        'values':data["seats"][data["region"]=="Central"],
-        "text":["Election"],
-      "textposition":"inside",
-      "domain": {"column": 1},
-      "name": "Party Trend",
-      "hoverinfo":"label+percent+name",
-      "hole": .4,
-      "type": "pie"},
-        {
-        'labels':data["party"][data["region"]=="north_east"],
-        'values':data["seats"][data["region"]=="north_east"],
-      "text":["Election"],
-      "textposition":"inside",
-      "domain": {"column": 1},
-      "name": "Party Trend",
-      "hoverinfo":"label+percent+name",
-      "hole": .4,
-      "type": "pie"}],
-        "layout": {
-        "title":"Lok sabaha election 2019",
-        "grid": {"rows": 1, "columns": 2},
-        "annotations": [
-            {
-                "font": {
-                    "size": 20
-                },
-                "showarrow": False,
-                "text": "GHG",
-                "x": 0.20,
-                "y": 0.5
-            },
-            {
-                "font": {
-                    "size": 20
-                },
-                "showarrow": False,
-                "text": "Parties",
-                "x": 0.8,
-                "y": 0.5
-            }
-        ]
-    }}
-plot(pie)
+region_wise_trend = region_wise_trend.sort_values("region")
+sns.barplot(region_wise_trend["party"][region_wise_trend["region"]=="North"], region_wise_trend["seats"])
+sns.barplot(region_wise_trend["party"][region_wise_trend["region"]=="East"], region_wise_trend["seats"])
+plt.xticks(rotation=90)
