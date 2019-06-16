b615d679bf5d178f25b762214f313e1ca1b432f0
@piyush546 <pk62893@gmail.com>
2019-06-16 14:34:40 +0530

    updated

--- Django_projects/testproject/Instagramcloneapp/views.py
+++ Django_projects/testproject/Instagramcloneapp/views.py
@@ -15,6 +15,7 @@ def signup_view(request):
         password = signup_form.cleaned_data["password"]
         user = UserModel(name=name, password=make_password(password), email = email, username= username)
         user.save()
+        return render(request, "success.html")
         
     return render(request, 'index.html',{'signup_form': signup_form})
     
