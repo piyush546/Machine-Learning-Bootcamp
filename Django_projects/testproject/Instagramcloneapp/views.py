6bfcb9ceee2aaf3831168f68f2055e1480950116
@piyush546 <pk62893@gmail.com>
2019-06-16 14:09:36 +0530

    updated

--- Django_projects/testproject/Instagramcloneapp/views.py
+++ Django_projects/testproject/Instagramcloneapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .forms import SignUpForm
+from models import UserModel
+from django.contrib.auth.hashers import make_password
 # Create your views here.
 def signup_view(request):
     signup_form = ''
@@ -7,6 +9,11 @@ def signup_view(request):
         signup_form  = SignUpForm()
     elif request.method == "POST":
         signup_form = SignUpForm(request.POST)
-            
+        username = signup_form.cleaned_data["username"]
+        name = signup_form.cleaned_data["name"]
+        email = signup_form.cleaned_data["email"]
+        password = signup_form.cleaned_data["password"]
+        user = UserModel(name=name, password=make_password(password))
+        
     return render(request, 'index.html',{'signup_form': signup_form})
     
