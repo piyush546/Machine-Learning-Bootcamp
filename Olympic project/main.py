10d4594af97e9944ba383b1475f73d9cf486dd6e
@piyush546 <pk62893@gmail.com>
2019-06-25 10:42:45 +0530

    updated

--- Olympic project/main.py
+++ Olympic project/main.py
@@ -14,4 +14,20 @@ init_notebook_mode(connected=True)
 # Loading the olympic data
 olym_data = pd.read_csv("olym_data.csv")
 
+# Splitting the data in the summer and winter olympics
+#summer_olympic = olym_data[olym_data["Season"]=="Summer"]
+#winter_olympic = olym_data[olym_data["Season"]=="Winter"]
 
+# Athelete count over years
+count = olym_data.groupby("Year")["Season"].value_counts().unstack()
+plt.style.use('seaborn')
+plt.scatter(count.index, count["Summer"])
+plt.plot(count.index, count["Summer"],"r-o")
+plt.xlim(1880, 2020)
+plt.ylim(0,16000)
+plt.savefig("Summer Athelete count.jpg")
+
+plt.style.use("seaborn")
+plt.scatter(count.index, count["Winter"])
+plt.plot(count.index, count["Winter"],"r-o")
+plt.savefig("Winter Athelete count.jpg")
