434d1b656bdeafe7759652f54ebdb88e5aeb2fc3
@piyush546 <pk62893@gmail.com>
2019-06-25 09:47:42 +0530

    updated

---
+++ Olympic project/main.py
@@ -0,0 +1,15 @@
+# This project is to describe about the various aspects of Olympic games.
+
+# Stage 1-
+# Importing the Data Preprocessing and visualization modules
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns
+from plotly.offline import init_notebook_mode, plot
+import plotly.graph_objs as go
+init_notebook_mode(connected=True)
+
+
+# Loading the olympic data
+olym_data = pd.read_csv("olym.csv")
