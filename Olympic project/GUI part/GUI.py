f1930a08098bf50d076e84b3a4d6dbb7eaf1853e
@piyush546 <pk62893@gmail.com>
2019-07-13 20:10:57 +0530

    updated

--- Olympic project/GUI part/GUI.py
+++ Olympic project/GUI part/GUI.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 # Step3 ----> Functional GUI
-from flask import Flask, render_templates, request, url_for
-from data import years
+from flask import Flask, render_template, request, url_for
+from data import years, result
 
 app = Flask(__name__)
 
@@ -10,9 +10,15 @@ app = Flask(__name__)
 def home():
     season = request.form.get("Season")
     year = years(season)
-    return render_templates("home.html", year=year)
+    return render_template("home.html", year=year)
 
 @app.route("/medal_tally")
 def medal_tally():
     data = request.form
-    Season = data["]
+    Season = data["Season"]
+    year = int(data["year"])
+    tally = result(Season, year)
+    return render_template("result.html", data=tally.to_html())
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- Olympic project/GUI part/data.py
+++ Olympic project/GUI part/data.py
@@ -10,3 +10,13 @@ def years(Season):
     data = data_mod()
     unique_years = data.Year[data["Season"]==Season].unique()
     return unique_years
+
+def result(S, y):
+    data = data_mod()
+    result = data[(data["Season"]==S) &(data["Year"]==y)]
+    result = result.groupby("NOC")["Medal"].value_counts().unstack()
+    result = result.iloc[:[1,2,0]]
+    result = result.fillna(0)
+    result["Total"] = result.apply(lambda x: sum(x), axis=1)
+    result = result.sort_values("Gold")
+    return result
